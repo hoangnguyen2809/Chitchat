ee91634d5fe6ca28b536ef3d3128a96a66ee1e68
anhhoang289 <viethoangthp289@gmail.com>
2024-06-14 19:40:45 -0700

    Handle user connection, maintain user list

--- server.go
+++ server.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/websocket"
 )
@@ -67,6 +70,14 @@ func (s *Server) HandleConnection(w http.ResponseWriter, r *http.Request) {
 	}
 
     log.Printf("Client connected: %s", c.RemoteAddr().String()) 
+	err = c.WriteMessage(websocket.TextMessage, []byte("Welcome to the chat server! \n Please enter your name:"))
+	if err != nil {
+		log.Println("write:", err)
+	}
+	_, msg, err := c.ReadMessage()
+	log.Printf("Received client's username: %s", msg)
+	client := &Client{name: string(msg), conn: c, send: make(chan []byte, 256)}
+	s.register <- client
 
 	// loop to read messages from the client 
 	for {
@@ -77,13 +88,27 @@ func (s *Server) HandleConnection(w http.ResponseWriter, r *http.Request) {
 		}
 		log.Printf("Received message from client: %s", msg)
 
-		err = c.WriteMessage(websocket.TextMessage, msg)
-		if err != nil {
-			log.Println("write:", err)
-			break
+		//handle the message
+		switch string(msg) {
+		case "list rooms":
+			err = c.WriteMessage(websocket.TextMessage, []byte(s.listRoom()))
+			if err != nil {
+				log.Println("write:", err)
+			}
+		case "list clients":
+			err = c.WriteMessage(websocket.TextMessage, []byte(s.listClients(client)))
+			if err != nil {
+				log.Println("write:", err)
+			}
 		}
+		// err = c.WriteMessage(websocket.TextMessage, msg)
+		// if err != nil {
+		// 	log.Println("write:", err)
+		// 	break
+		// }
 	}
-	
+
+	s.unregister <- client
     log.Printf("Client disconnected: %s", c.RemoteAddr().String())
 }
 
@@ -96,6 +121,21 @@ func (s *Server) listRoom() string {
 	return roomList
 }
 
+func (s *Server) listClients(client *Client) string {
+	clientCount := len(s.clients)
+	clientList := fmt.Sprintf("There are currently %d clients connected:\n", clientCount)
+
+	for _, c := range s.clients {
+		if client != nil && c == client {
+			clientList += c.name + " (You)\n"
+		} else {
+			clientList += c.name + "\n"
+		}
+	}
+
+	return clientList
+}
+
 func main() {
 	flag.Parse()
 	log.SetFlags(0)
@@ -103,10 +143,23 @@ func main() {
 	go server.Run()
 	http.HandleFunc("/", server.HandleConnection)
 	log.Printf("Starting server on %s", *addr)
-	err := (http.ListenAndServe(*addr, nil))
-	if err != nil {
-		log.Fatalf("Failed to start server: %v", err)
-	} else {
-		log.Println("Server started, waiting for connection")
+	go func ()  {
+		err := (http.ListenAndServe(*addr, nil))
+		if err != nil {
+			log.Fatalf("Failed to start server: %v", err)
+		} else {
+			log.Println("Server started, waiting for connection")
+		}
+	}()
+	
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		text := scanner.Text()
+		switch text {
+		case "list rooms":
+			log.Println(server.listRoom())
+		case "list clients":
+			log.Println(server.listClients(nil))
+		}
 	}
 }
